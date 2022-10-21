3bc0d56e9a362c6d30652ee30d6ccfd6d7ff062d
marius93rm <marius93rm@gmail.com>
2022-10-21 13:05:42 +0200

    to update

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Root = createNativeStackNavigator();
+
+const Homepage = ( {navigation, route} ) => {
+  return (
+    <View style={styles.container}>
+      <Text>Homepage</Text>
+      <Button title="Clicca per informazioni" onPress={() => {navigation.push("Informazioni")}} />
+    </View>
+  );
+}
+
+
+
+const Informazioni = ({navigation, route}) => {
+  return (
+    <View style={styles.container}>
+      <Text>{route.name}</Text>
+      <Button title="Clicca per tornare alla homepage" onPress={() => {navigation.pop()}} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Root.Navigator>
+        <Root.Screen name="Homepage" component={Homepage} />
+        <Root.Screen name="Informazioni" component={Informazioni} />
+      </Root.Navigator>
+    </NavigationContainer>
   );
 }
 
